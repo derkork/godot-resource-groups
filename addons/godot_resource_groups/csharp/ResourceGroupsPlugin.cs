83dc51752345281e84c3d2d03b89a5a422b0d1b8
Jan Thomä <janthomae@janthomae.de>
2025-05-09 08:48:20 +0200

    feat: add C# support

---
+++ addons/godot_resource_groups/csharp/ResourceGroupsPlugin.cs
@@ -0,0 +1,28 @@
+﻿using Godot;
+
+namespace GodotResourceGroups;
+
+public class ResourceGroupsPlugin
+{
+    /// <summary>
+    /// Rebuilds all resource groups. Useful if you have custom editor scripts that create or modify resources
+    /// and want to update the resource groups via script.
+    /// </summary>
+    public static void RebuildResourceGroups()
+    {
+        if (!Engine.IsEditorHint())
+		{
+			GD.PushError("ResourceGroupsPlugin.RebuildResourceGroups() can only be called inside of the editor.");
+			return;
+		}
+        
+        var plugin = Engine.GetSingleton("ResourceGroupsPlugin");
+        if (plugin == null)
+        {
+	        GD.PushError("Resource group plugin is not active, please check if it is enabled in the project settings.");
+	        return;
+        }
+
+        plugin.Call("_rebuild_resource_groups");
+    }
+}
